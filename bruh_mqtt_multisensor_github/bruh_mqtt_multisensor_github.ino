2186ce7f3ec08b3b7b1ac622fdcdca354fe5e41a
Chris <firstof9@gmail.com>
2017-11-28 16:03:25 -0700

    Should support all MQ sensors
    Returns in PPM
    * LPG
    * CO
    * Smoke

--- bruh_mqtt_multisensor_github/bruh_mqtt_multisensor_github.ino
+++ bruh_mqtt_multisensor_github/bruh_mqtt_multisensor_github.ino
@@ -38,13 +38,48 @@
  * Uncomment to enable MQ type sensor
 */
  
-//#define MY_MQ2
-//#define MY_MQ135
+//#define ENABLE_MQ
 
-#if defined(MY_MQ2)
-#include <MQ2.h>
-#elif defined(MY_MQ135)
-#include <MQ135.h>
+#ifdef ENABLE_MQ
+int lpg, co, smoke;
+int diffLPG = 1;
+int diffCO = 1;
+int diffSmoke = 1;
+
+/************************Hardware Related Macros************************************/
+#define         MQ_SENSOR_ANALOG_PIN         (2)     //define which analog input channel you are going to use
+#define         RL_VALUE                     (5)     //define the load resistance on the board, in kilo ohms
+#define         RO_CLEAN_AIR_FACTOR          (9.83)  //RO_CLEAR_AIR_FACTOR=(Sensor resistance in clean air)/RO,
+//which is derived from the chart in datasheet
+/***********************Software Related Macros************************************/
+#define         CALIBARAION_SAMPLE_TIMES     (50)    //define how many samples you are going to take in the calibration phase
+#define         CALIBRATION_SAMPLE_INTERVAL  (500)   //define the time interal(in milisecond) between each samples in the
+//cablibration phase
+#define         READ_SAMPLE_INTERVAL         (50)    //define how many samples you are going to take in normal operation
+#define         READ_SAMPLE_TIMES            (5)     //define the time interal(in milisecond) between each samples in
+//normal operation
+/**********************Application Related Macros**********************************/
+#define         GAS_LPG                      (0)
+#define         GAS_CO                       (1)
+#define         GAS_SMOKE                    (2)
+/*****************************Globals***********************************************/
+//VARIABLES
+float Ro = 10000.0;    // this has to be tuned 10K Ohm
+int val = 0;           // variable to store the value coming from the sensor
+float valMQ =0.0;
+float lastMQ =0.0;
+float           LPGCurve[3]  =  {2.3,0.21,-0.47};   //two points are taken from the curve.
+//with these two points, a line is formed which is "approximately equivalent"
+//to the original curve.
+//data format:{ x, y, slope}; point1: (lg200, 0.21), point2: (lg10000, -0.59)
+float           COCurve[3]  =  {2.3,0.72,-0.34};    //two points are taken from the curve.
+//with these two points, a line is formed which is "approximately equivalent"
+//to the original curve.
+//data format:{ x, y, slope}; point1: (lg200, 0.72), point2: (lg10000,  0.15)
+float           SmokeCurve[3] = {2.3,0.53,-0.44};   //two points are taken from the curve.
+//with these two points, a line is formed which is "approximately equivalent"
+//to the original curve.
+//data format:{ x, y, slope}; point1: (lg200, 0.53), point2:(lg10000,-0.22)
 #endif
 
 
@@ -116,19 +151,6 @@ const int BUFFER_SIZE = 300;
 
 #define MQTT_MAX_PACKET_SIZE 512
 
-#if defined(MY_MQ2)
-int lpg, co, smoke;
-int diffLPG = 1;
-int diffCO = 1;
-int diffSmoke = 1;
-
-#elif defined(MY_MQ135)
-float diffAQ = 0.1; // sensitivity to report new update
-float airQuality;
-#endif
-
-
-
 /******************************** GLOBALS for fade/flash *******************************/
 byte red = 255;
 byte green = 255;
@@ -163,13 +185,6 @@ byte flashBrightness = brightness;
 WiFiClient espClient;
 PubSubClient client(espClient);
 DHT dht(DHTPIN, DHTTYPE);
-
-
-#if defined(MY_MQ2)
-MQ2 mq2(MQ2PIN);
-#elif defined(MY_MQ135)
-MQ135 gasSensor = MQ135(MQ135PIN);
-#endif
 
 /********************************** START SETUP*****************************************/
 void setup() {
@@ -390,14 +405,11 @@ void sendState() {
   root["ldr"] = (String)LDR;
   root["temperature"] = (String)tempValue;
   root["heatIndex"] = (String)calculateHeatIndex(humValue, tempValue);
-  #if defined(MY_MQ2)
+  #ifdef ENABLE_MQ
   root["lpg"] = (String)lpg;
   root["co"] = (String)co;
   root["smoke"] = (String)smoke;
-  #elif defined(MY_MQ135)
-  root["airquality"] = (String)airQuality;
   #endif
-
   char buffer[root.measureLength() + 1];
   root.printTo(buffer, sizeof(buffer));
 
@@ -493,10 +505,26 @@ void loop() {
     float newTempValue = dht.readTemperature(true); //to use celsius remove the true text inside the parentheses  
     float newHumValue = dht.readHumidity();
     
-    #if defined(MY_MQ2)
-    int newLPG = mq2.readLPG();
-    int newCO = mq2.readCO();
-    int newSmoke = mq2.readSmoke();
+    #ifdef ENABLE_MQ
+    uint16_t co = MQGetGasPercentage(MQRead(MQ_SENSOR_ANALOG_PIN)/Ro,GAS_CO);
+    uint16_t lpg = MQGetGasPercentage(MQRead(MQ_SENSOR_ANALOG_PIN)/Ro,GAS_LPG);
+    uint16_t smoke = MQGetGasPercentage(MQRead(MQ_SENSOR_ANALOG_PIN)/Ro,GAS_SMOKE);
+    #ifdef MY_DEBUG
+    Serial.println(val);
+  
+    Serial.print("LPG:");
+    Serial.print(MQGetGasPercentage(MQRead(MQ_SENSOR_ANALOG_PIN)/Ro,GAS_LPG) );
+    Serial.print( "ppm" );
+    Serial.print("    ");
+    Serial.print("CO:");
+    Serial.print(MQGetGasPercentage(MQRead(MQ_SENSOR_ANALOG_PIN)/Ro,GAS_CO) );
+    Serial.print( "ppm" );
+    Serial.print("    ");
+    Serial.print("SMOKE:");
+    Serial.print(MQGetGasPercentage(MQRead(MQ_SENSOR_ANALOG_PIN)/Ro,GAS_SMOKE) );
+    Serial.print( "ppm" );
+    Serial.print("\n");
+    #endif
 
     if (checkBoundSensor(newLPG, lpg, diffLPG)) {
       lpg = newLPG;
@@ -510,17 +538,8 @@ void loop() {
       smoke = newSmoke;
       sendState();
     }
-    
-    #elif defined(MY_MQ135)
-    
-    float newAirQuality = gasSensor.getPPM(); // read out of the gas sensor
 
-    if (checkBoundSensor(newAirQuality, airQuality, diffAQ)) {
-      airQuality = newAirQuality;
-      sendState();
-    }
-    
-    #endif
+    #endif  
 
     
     //PIR CODE
@@ -701,3 +720,98 @@ void software_Reset() // Restarts program from beginning but does not reset the 
 Serial.print("resetting");
 ESP.reset(); 
 }
+
+#ifdef ENABLE_MQ
+/****************** MQResistanceCalculation ****************************************
+Input:   raw_adc - raw value read from adc, which represents the voltage
+Output:  the calculated sensor resistance
+Remarks: The sensor and the load resistor forms a voltage divider. Given the voltage
+         across the load resistor and its resistance, the resistance of the sensor
+         could be derived.
+************************************************************************************/
+float MQResistanceCalculation(int raw_adc)
+{
+    return ( ((float)RL_VALUE*(1023-raw_adc)/raw_adc));
+}
+
+/***************************** MQCalibration ****************************************
+Input:   mq_pin - analog channel
+Output:  Ro of the sensor
+Remarks: This function assumes that the sensor is in clean air. It use
+         MQResistanceCalculation to calculates the sensor resistance in clean air
+         and then divides it with RO_CLEAN_AIR_FACTOR. RO_CLEAN_AIR_FACTOR is about
+         10, which differs slightly between different sensors.
+************************************************************************************/
+float MQCalibration(int mq_pin)
+{
+    int i;
+    float val=0;
+
+    for (i=0; i<CALIBARAION_SAMPLE_TIMES; i++) {          //take multiple samples
+        val += MQResistanceCalculation(analogRead(mq_pin));
+        delay(CALIBRATION_SAMPLE_INTERVAL);
+    }
+    val = val/CALIBARAION_SAMPLE_TIMES;                   //calculate the average value
+
+    val = val/RO_CLEAN_AIR_FACTOR;                        //divided by RO_CLEAN_AIR_FACTOR yields the Ro
+    //according to the chart in the datasheet
+
+    return val;
+}
+/*****************************  MQRead *********************************************
+Input:   mq_pin - analog channel
+Output:  Rs of the sensor
+Remarks: This function use MQResistanceCalculation to caculate the sensor resistenc (Rs).
+         The Rs changes as the sensor is in the different consentration of the target
+         gas. The sample times and the time interval between samples could be configured
+         by changing the definition of the macros.
+************************************************************************************/
+float MQRead(int mq_pin)
+{
+    int i;
+    float rs=0;
+
+    for (i=0; i<READ_SAMPLE_TIMES; i++) {
+        rs += MQResistanceCalculation(analogRead(mq_pin));
+        delay(READ_SAMPLE_INTERVAL);
+    }
+
+    rs = rs/READ_SAMPLE_TIMES;
+
+    return rs;
+}
+
+/*****************************  MQGetGasPercentage **********************************
+Input:   rs_ro_ratio - Rs divided by Ro
+         gas_id      - target gas type
+Output:  ppm of the target gas
+Remarks: This function passes different curves to the MQGetPercentage function which
+         calculates the ppm (parts per million) of the target gas.
+************************************************************************************/
+int MQGetGasPercentage(float rs_ro_ratio, int gas_id)
+{
+    if ( gas_id == GAS_LPG ) {
+        return MQGetPercentage(rs_ro_ratio,LPGCurve);
+    } else if ( gas_id == GAS_CO ) {
+        return MQGetPercentage(rs_ro_ratio,COCurve);
+    } else if ( gas_id == GAS_SMOKE ) {
+        return MQGetPercentage(rs_ro_ratio,SmokeCurve);
+    }
+
+    return 0;
+}
+
+/*****************************  MQGetPercentage **********************************
+Input:   rs_ro_ratio - Rs divided by Ro
+         pcurve      - pointer to the curve of the target gas
+Output:  ppm of the target gas
+Remarks: By using the slope and a point of the line. The x(logarithmic value of ppm)
+         of the line could be derived if y(rs_ro_ratio) is provided. As it is a
+         logarithmic coordinate, power of 10 is used to convert the result to non-logarithmic
+         value.
+************************************************************************************/
+int  MQGetPercentage(float rs_ro_ratio, float *pcurve)
+{
+    return (pow(10,( ((log(rs_ro_ratio)-pcurve[1])/pcurve[2]) + pcurve[0])));
+}
+#endif
